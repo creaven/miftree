9f53da055aef4548afddbb24f077146ef5a97954
creavenmoro <creavenmoro@5e8ecd4a-8f4d-0410-8c2a-0deb901ad9b7>
2009-11-11 08:03:59 +0000

    fix position patch, it's required for old mootools versions

--- Source/Core/Mif.Tree.mootools-patch.js
+++ Source/Core/Mif.Tree.mootools-patch.js
@@ -1,17 +1,23 @@
 /*mootools patch*/
 
 if(document.documentElement.getBoundingClientRect){//ie, opear9.5+, ff3+
-
+	
 	Element.implement({
 
-		getPosition: function(relative){
-			rect=this.getBoundingClientRect();
-			var clientTop = document.html.clientTop || document.body.clientTop || 0, clientLeft = document.html.clientLeft || document.body.clientLeft || 0
-			var position={x: rect.left-this.scrollLeft-clientLeft, y:rect.top-this.scrollTop-clientTop};
-			var relativePosition = (relative && (relative = $(relative))) ? relative.getPosition() : {x: 0, y: 0};
-			return {x: position.x - relativePosition.x, y: position.y - relativePosition.y};
+		getOffsets: function(){
+			var bound = this.getBoundingClientRect(),
+				html = $(this.getDocument().documentElement),
+				htmlScroll = html.getScroll(),
+				elemScrolls = this.getScrolls(),
+				elemScroll = this.getScroll(),
+				isFixed = (this.getComputedStyle('position') == 'fixed');
+
+			return {
+				x: bound.left.toInt() + elemScrolls.x - elemScroll.x + ((isFixed) ? 0 : htmlScroll.x) - html.clientLeft,
+				y: bound.top.toInt()  + elemScrolls.y - elemScroll.y + ((isFixed) ? 0 : htmlScroll.y) - html.clientTop
+			};
 		}
-		
+	
 	});
 
 }
